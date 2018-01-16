dab4ebda8156e20a11eaccac2c6245b77e772891
Christian Zellweger <chzellweger@gmail.com>
2018-01-16 23:15:06 +0100

    implement simple redux example

--- stripped/index.js
+++ stripped/index.js
@@ -1,6 +1,28 @@
 import { createStore } from 'redux'
 
-// reducer
+/*
+* actions
+*/
+
+// we use action creators
+// action-creators are simple functions that return an action-object
+// and allow for easy parameterization
+// we could also use just plain objects like { type: 'DECREMENT', value: 1}
+
+function doIncrement(value) {
+  return { type: 'INCREMENT', value }
+}
+
+let doDecrement = value => {
+  return { type: 'DECREMENT', value }
+}
+
+/*
+* reducer
+*/
+// knows how to handle an action object
+// takes the current state and an action
+// returns a new state
 export function counter(state = 0, action) {
   switch (action.type) {
     case 'INCREMENT':
@@ -12,32 +34,37 @@ export function counter(state = 0, action) {
   }
 }
 
+/*
+* store
+*/
+// the store is a simple object with methods
+// at creation, pass it the reducer, so it knows how 
+// to handle the actions passed later by store.dispatch()
+
 // create the store
 let store = createStore(
+  // pass the reducer:
   counter,
+  // just to hook up the redux devtools:
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-// actions
-function doIncrement(value) {
-  return { type: 'INCREMENT', value }
-}
-
-let doDecrement = value => {
-  return { type: 'DECREMENT', value }
-}
-
-// html-setup
-const INTERVAL = 1
+/*
+* attach click handler
+*/
 
 document.querySelector('#increment').addEventListener('click', () => {
-  store.dispatch(doIncrement(INTERVAL))
+  // dispatch the action to the reducer
+  // could also be: store.dispatch({ type: 'INCREMENT', value: 1 })
+  store.dispatch(doIncrement(1))
 })
 
 document.querySelector('#decrement').addEventListener('click', () => {
-  store.dispatch(doDecrement(INTERVAL))
+  // dispatch the action to the reducer
+  store.dispatch(doDecrement(1))
 })
 
+// listen for change events from the store object
 store.subscribe(() => {
-  document.querySelector('#result').innerHTML = store.getState()
+  document.querySelector('#result').innerHTML = store.getState() // .getState() returns the current state
 })
